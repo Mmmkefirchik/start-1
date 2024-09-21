efa76ebc9ea165c322c77a13725db0f4344a8a21
Kirill <mail@mail.com>
2024-09-21 08:53:20 +0300

    fyvfhvfhfhkgukl

--- urok_8.py
+++ urok_8.py
@@ -21,5 +21,13 @@ y=50
 
 wrap.sprite.add('pacman',x,y,'enemy_pink_up1')
 
-
 wrap.sprite.add('pacman',width-50,height-50,'enemy_ill_white1')
+
+wrap.sprite.add('pacman',width-50,height-height+50,'enemy_red_left1')
+
+
+wrap.sprite.add('pacman',width-300,height-180,'dot')
+
+wrap.sprite.add('pacman',width-width+300,height-180,'dot')
+
+wrap.sprite.add('pacman',width-width+500,height-height+100,'dot')
