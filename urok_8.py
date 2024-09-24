805e54544896df1f15bce89e4bc5a38eace9ba1a
Kirill <mail@mail.com>
2024-09-24 20:42:06 +0300

    fyvfhvfhfhkgukl

--- urok_8.py
+++ urok_8.py
@@ -1,3 +1,5 @@
+import time
+
 import wrap
 
 width=900
@@ -5,13 +7,18 @@ height=700
 
 x=50
 y=height-50
+x1=300
+y1=50
+x2=600
+y2=520
+x3=100
+y3=300
+x4=width/2
+y4=height/2
 
 wrap.world.create_world(width,height)
 
-wrap.sprite.add('pacman',x,y,'enemy_blue_down1')
-
-x=50
-y=50
+wrap.sprite.add('pacman',50,50,'enemy_blue_down1')
 
 wrap.sprite.add('pacman',x,y,'enemy_pink_up1')
 
@@ -19,14 +26,21 @@ wrap.sprite.add('pacman',width-50,height-50,'enemy_ill_white1')
 
 wrap.sprite.add('pacman',width-50,height-height+50,'enemy_red_left1')
 
-q=width-300
-w=height-180
-wrap.sprite.add('pacman',q,w,'dot')
+wrap.sprite.add('pacman',x1,y1,'dot')
 
-a=width-width+300
-b=height-180
-wrap.sprite.add('pacman',a,b,'dot')
+wrap.sprite.add_text('Точка1',x1,y1-25,text_color=[255,255,255])
 
-e=width-width+500
-r=height-height+100
-wrap.sprite.add('pacman',e,r,'dot')
+wrap.sprite.add('pacman',x2,y2,'dot')
+wrap.sprite.add_text('Точка3',x2,y2-25,text_color=[255,255,255])
+
+wrap.sprite.add('pacman',x3,y3,'dot')
+wrap.sprite.add_text('Точка2',x3,y3-25,text_color=[255,255,255])
+
+wrap.sprite.add('pacman',x4,y4,'player1')
+time.sleep(1.5)
+
+wrap.actions.move_to(10,x1,y1)
+
+
+
+
