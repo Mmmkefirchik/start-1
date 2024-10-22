43965266334152f63cfb8056ecc1cc38aaa9b5f4
Kirill <mail@mail.com>
2024-10-22 20:35:33 +0300

    fyvfhvfhfhkgukl

--- urok_11.py
+++ urok_11.py
@@ -1,4 +1,5 @@
 import random,wrap,time
+
 d=random.randint(0,100)
 wrap.world.create_world(1000,1000)
 mesto_tochci_x=random.randint(0, 1000)
@@ -17,21 +18,19 @@ rasmer_pacman=random.randint(32, 120)
 wrap.sprite.set_height_proportionally(nomer_pacmana, rasmer_pacman)
 height_pacman=wrap.sprite.get_height(nomer_pacmana)
 
-
 wrap.actions.set_angle_to_point(nomer_pacmana, mesto_tochci_x, mesto_tochci_y)
 time.sleep(0.5)
 wrap.actions.move_to(nomer_pacmana, mesto_tochci_x, mesto_tochci_y)
 
-
+rasmer_privideniya=random.randint(32, 300)
 nomer_prividenie=wrap.sprite.add('pacman',0,0,'enemy_red_right1',False)
 
-height_prividenie=wrap.sprite.get_height(nomer_prividenie)
-mesto_privideniya_y= mesto_tochci_y - (height_pacman // 2 + height_prividenie)
+mesto_privideniya_y=mesto_tochci_y-height_pacman//2-20-rasmer_privideniya//2
 
 mesto_privideniya_x=random.randint(mesto_tochci_x-50,mesto_tochci_x+50)
 
 nomer_prividenie=wrap.sprite.add('pacman', mesto_privideniya_x, mesto_privideniya_y, 'enemy_red_right1')
-rasmer_privideniya=random.randint(26, 100)
+# rasmer_privideniya=random.randint(300, 300)
 wrap.sprite.set_height_proportionally(nomer_prividenie, rasmer_privideniya)
 height_prividenie=wrap.sprite.get_height(nomer_prividenie)
 
@@ -40,5 +39,10 @@ mesto_texta_y= mesto_privideniya_y - (height_prividenie // 2 + 20)
 wrap.sprite.add_text('Ага, попался!', mesto_privideniya_x, mesto_texta_y, text_color=[255, 255, 255])
 
 
+gradys_privedenie=wrap.sprite.calc_angle_by_point(nomer_pacmana,mesto_privideniya_x,mesto_privideniya_y)
+gradys_pacman=gradys_privedenie-180
+
+wrap.actions.set_angle(nomer_pacmana,gradys_pacman)
 
 
+wrap.actions.move_at_angle_dir(nomer_pacmana,100)
