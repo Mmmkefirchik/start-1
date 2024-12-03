deefc6423918575a7c5d8106b1e8b706c12417e8
Kirill <mail@mail.com>
2024-12-03 20:39:50 +0300

    fyvfhvfhfhkgukl

---
+++ vishna.py
@@ -0,0 +1,48 @@
+import random
+
+vsego=random.randint(0,1)
+
+if vsego==0:
+    print('Вы получили 0 вишен')
+
+elif vsego==1:
+    print('Вы получили 1 вишню')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
