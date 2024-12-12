21d6eadadaf25094cbd4b0b66026c707bcae0a21
Kirill <mail@mail.com>
2024-12-12 19:12:45 +0300

    fyvfhvfhfhkgukl

--- while.py
+++ while.py
@@ -1,9 +1,22 @@
 import random
 
-rezoltat=random.randint(1,10)#1
+rezoltat=random.randint(1,10000)#1
 otvet=-1000
+
+popatka = 0
+# if
 while rezoltat!=otvet:
-    otvet=input('пароль?')
+    popatka = popatka + 1
+    otvet = input(str(popatka)+') пароль?')
+
+    if otvet.isnumeric()==False:
+        continue
     otvet=int(otvet)
+
+    if rezoltat>otvet:
+        print('Больше')
+
+    if rezoltat<otvet:
+        print('Меньше')
 else:
    print('вы угадали')
