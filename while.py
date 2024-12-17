44c440e2c64cbe471a8ee761e91860acddfb4911
Kirill <mail@mail.com>
2024-12-17 20:48:03 +0300

    fyvfhvfhfhkgukl

--- while.py
+++ while.py
@@ -1,22 +1,35 @@
 import random
-
-rezoltat=random.randint(1,10000)#1
+vsego=10000
+rezoltat=random.randint(1, vsego)#1
 otvet=-1000
 
-popatka = 0
-# if
+popatka = 1
+
 while rezoltat!=otvet:
-    popatka = popatka + 1
     otvet = input(str(popatka)+') пароль?')
+    # a='СДАЮСЬ'
+    if otvet.upper()=='СДАЮСЬ':
+        print('Я загадала число - ' + str(rezoltat))
+        break
 
     if otvet.isnumeric()==False:
+        print('Пишите цифры')
         continue
+    # popatka = popatka + 1
     otvet=int(otvet)
 
+    if otvet>vsego:
+        print('Макс. числ.- ' + str(vsego))
+        continue
+    # else:
+
+    # if otvet<=rezoltat:
     if rezoltat>otvet:
         print('Больше')
 
     if rezoltat<otvet:
         print('Меньше')
+    popatka = popatka + 1
+
 else:
-   print('вы угадали')
+   print('вы угадали за '+str(popatka-1)+' попитка')
