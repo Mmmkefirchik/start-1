59ad4c77bb8d04c90e81105354b7beb3a7e98593
Kirill <mail@mail.com>
2024-12-21 08:48:24 +0300

    fyvfhvfhfhkgukl

--- bronenosec.py
+++ bronenosec.py
@@ -7,11 +7,13 @@ wrap.world.create_world(1000, 1000)
 z_x = random.randint(10, 900)
 z_y = random.randint(10, 900)
 
-nomer_s=wrap.sprite.add('battle_city_items', 550, 550, 'block_gift_star')
+nomer_s=wrap.sprite.add('battle_city_items', z_x, z_y, 'block_gift_star')
 nomer_a=wrap.sprite.add('mario-enemies', 500, 500, 'armadillo_egg')
 a = 1
 
 while True:
+        # z_x = random.randint(0, 900)
+        # z_y = random.randint(0, 900)
         wrap.sprite.move_at_angle_point(nomer_a,z_x,z_y,6)
         wrap.sprite.set_angle(nomer_a,a)
         a = a + 4
@@ -19,6 +21,6 @@ while True:
         # z_y = random.randint(10, 900)
 
         if sprite.sprites_collide(nomer_s,nomer_a):
-            x=random.randint(0,900)
-            y=random.randint(0,900)
-            wrap.sprite.move_to(nomer_s,x,y)
+            z_x=random.randint(0,900)
+            z_y=random.randint(0,900)
+            wrap.sprite.move_to(nomer_s,z_x,z_y)
