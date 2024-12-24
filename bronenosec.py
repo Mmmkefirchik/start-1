13650579913af0efcced08eedd0cb6d9465ded70
Kirill <mail@mail.com>
2024-12-24 20:41:33 +0300

    fyvfhvfhfhkgukl

--- bronenosec.py
+++ bronenosec.py
@@ -1,4 +1,4 @@
-import random, wrap
+import random, wrap,time
 
 from wrap_py import sprite
 
@@ -10,17 +10,31 @@ z_y = random.randint(10, 900)
 nomer_s=wrap.sprite.add('battle_city_items', z_x, z_y, 'block_gift_star')
 nomer_a=wrap.sprite.add('mario-enemies', 500, 500, 'armadillo_egg')
 a = 1
+d=1
+i = time.time()
+e = time.time() - i
+e=int(e)
+q = wrap.sprite.add_text(str(e), 500, 500, text_color=[255, 255, 255])
 
 while True:
-        # z_x = random.randint(0, 900)
-        # z_y = random.randint(0, 900)
+        # i=time.time()
+        e=time.time()-i
+        e = int(e)
+
+        # print(e)
+        # print(d)
+        # q=wrap.sprite.add_text(str(e),500,500,text_color=[255,255,255])
+        wrap.sprite_text.set_text(q,str(e))
+
         wrap.sprite.move_at_angle_point(nomer_a,z_x,z_y,6)
         wrap.sprite.set_angle(nomer_a,a)
         a = a + 4
-        # z_x = random.randint(10, 900)
-        # z_y = random.randint(10, 900)
 
         if sprite.sprites_collide(nomer_s,nomer_a):
             z_x=random.randint(0,900)
             z_y=random.randint(0,900)
             wrap.sprite.move_to(nomer_s,z_x,z_y)
+            d = d + 1
+
+            if d==5:
+                break
