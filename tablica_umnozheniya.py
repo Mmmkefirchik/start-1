aa0e51c9fee04fb77207a331982194a3c062225e
Kirill <mail@mail.com>
2025-01-30 19:12:58 +0300

    fyvfhvfhfhkgukl

--- tablica_umnozheniya.py
+++ tablica_umnozheniya.py
@@ -1,29 +1,34 @@
-import random
+import random,time
+
 
 print('Вас приветствует тестировщик таблиц!')
 papatka=1
 papatka2=1
+
 na_chto=random.randint(2,9)
 cacaya=random.randint(2,9)
-while papatka<10:
+time1=time.time()#10
+while papatka<6:
 
-
+    time2=time.time()#11
+    time_final=time2-time1
     my_otvet=input(str(papatka)+') Сколько будет '+str(cacaya)+'*'+str(na_chto)+'? ('+str(papatka2)+')')
-    papatka2=papatka2+1
-    if my_otvet.isnumeric() == True:
-        my_otvet=int(my_otvet)
-    else:
+    if my_otvet.isnumeric()==False:
         print('Пишите цифры!')
-    if cacaya*na_chto==my_otvet:
+        continue
+
+    if cacaya*na_chto==int(my_otvet):
         print('правильно')
-        # papatka=papatka+1
+        papatka=papatka+1
         na_chto = random.randint(2, 9)
         cacaya = random.randint(2, 9)
-        # papatka2=1
-
-    else:
+        papatka2=1
+    elif cacaya*na_chto!=int(my_otvet):
         print('неправильно')
+        papatka2=papatka2+1
 
-    if cacaya*na_chto==my_otvet and my_otvet.isnumeric()==False:
-        papatka=papatka+1
-        papatka2+papatka2+1
+
+if
+# time_final=int(time_final)
+# print('Время работы - '+str(time_final))
+#
