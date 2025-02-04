2424c5fe4b82bc66510d629dae73807a74752191
Kirill <mail@mail.com>
2025-02-04 20:40:32 +0300

    fyvfhvfhfhkgukl

--- tablica_umnozheniya.py
+++ tablica_umnozheniya.py
@@ -8,10 +8,8 @@ papatka2=1
 na_chto=random.randint(2,9)
 cacaya=random.randint(2,9)
 time1=time.time()#10
-while papatka<6:
+while papatka<2:
 
-    time2=time.time()#11
-    time_final=time2-time1
     my_otvet=input(str(papatka)+') Сколько будет '+str(cacaya)+'*'+str(na_chto)+'? ('+str(papatka2)+')')
     if my_otvet.isnumeric()==False:
         print('Пишите цифры!')
@@ -27,8 +25,16 @@ while papatka<6:
         print('неправильно')
         papatka2=papatka2+1
 
+time2 = time.time()  # 11
+time_final = 62#time2 - time1
+w = time_final%60
+g=str(time_final%60)
 
-if
-# time_final=int(time_final)
-# print('Время работы - '+str(time_final))
-#
+
+    # g=str(int(time_final))+' секунд'
+if int(time_final-g)%60==0:
+    # w=str(int(time_final))+ 'минут'
+    w=str(int(time_final))
+if time_final<60 or time_final%60!=0:
+    g=str(int(time_final%60))+ ' секунд'
+print('Время работы - '+str(w)+" минут"+' и '+str(g))
