2fd61892cffcfa4d358c71191bf3fdf0ac9d036c
Kirill <mail@mail.com>
2025-02-06 19:07:13 +0300

    fyvfhvfhfhkgukl

---
+++ def.py
@@ -0,0 +1,10 @@
+def pishi_rasskaz(name_1,o,):
+    print('Привет, '+name_1+'!')
+    print("Ты не знаешь где "+o+"!?")
+
+pishi_rasskaz("Степочка",'Антон')
+print("Вот и всееееееее")
+
+pishi_rasskaz('Вася',"Герасим")
+
+print("Вот и всееееееее")
