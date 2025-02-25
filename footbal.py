ec2228bbfe019a88283e3f75a44b531e39ebb0a9
Kirill <mail@mail.com>
2025-02-25 20:37:50 +0300

    fyvfhvfhfhkgukl

---
+++ footbal.py
@@ -0,0 +1,20 @@
+import wrap,random
+wrap.world.create_world(400, 600, 900, 50)
+
+def pas(pers_nomer):
+    if  :#кто куда повернут...
+
+    # wrap.actions.move_to()низ сторона...
+
+
+m1=wrap.sprite.add('mario-1-big',300,250,'stand')
+m2=wrap.sprite.add('mario-2-big',100,250,'stand')
+# c=wrap.sprite.add('mario-enemies',200,400,'crab')
+a=wrap.sprite.add('mario-enemies',200,300,'armadillo_egg')
+# rezultat=random.choice([m1,m2,c])
+
+pas(m1)
+
+
+
+
