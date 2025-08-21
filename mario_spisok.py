875e4ed9f0d32da6b1e5748540657799c4b1ab97
Kirill <mail@mail.com>
2025-08-21 19:19:17 +0300

    fyvfhvfhfhkgukl

--- mario_spisok.py
+++ mario_spisok.py
@@ -1,13 +1,37 @@
 import wrap
 
-wrap.world.create_world(500,500)
+
+w=500
+wrap.world.create_world(w,500)
 wrap.world.set_back_color(98,149,248)
 
-a=[]
 e = wrap.sprite.add('mario-enemies',1000 , 450, 'cloud')
 c = wrap.sprite.get_width(e)
-t=0.5
-for b in range(100):
+
+for b in range(c//2,w,c):
     # wrap.sprite.move_to(e,c,0)
-    a.append(wrap.sprite.add('mario-enemies',c*t, 450, 'cloud'))
-    t=t+1
+    wrap.sprite.add('mario-enemies',b, 450, 'cloud')
+
+q=wrap.sprite.add('mario-scenery',1000,450,'heaven_cloud')
+i = wrap.sprite.get_width(q)//2
+start=150
+for r in range(start, i * 6 + start, i):
+    m=wrap.sprite.add('mario-scenery',r, 250, 'heaven_cloud')
+    wrap.sprite.set_size_percent(m,50,50)
+
+s=wrap.sprite.add('mario-items',1000,450,'block_bricks')
+height_sten=wrap.sprite.get_height(s)
+for f in range(100,height_sten*8+100,height_sten):
+    wrap.sprite.add('mario-items',100,f,'block_bricks')
+
+
+m=wrap.sprite.add('mario-items',1000,450,'coin')
+width_m=wrap.sprite.get_width(m)
+for y in range(width_m,w,width_m*2):
+    n=width_m*5
+    m = wrap.sprite.add('mario-items', y, 200, 'coin')
+    g=wrap.sprite.get_x(m)
+    if n == g:
+        continue
+
+
