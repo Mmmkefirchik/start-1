4ce1af1df81d156a91d31d54794f758f30be8fa1
Kirill <mail@mail.com>
2025-08-22 13:48:26 +0300

    fyvfhvfhfhkgukl

--- mario_spisok.py
+++ mario_spisok.py
@@ -14,10 +14,15 @@ for b in range(c//2,w,c):
 
 q=wrap.sprite.add('mario-scenery',1000,450,'heaven_cloud')
 i = wrap.sprite.get_width(q)//2
-start=150
+start=300
+n=0
 for r in range(start, i * 6 + start, i):
-    m=wrap.sprite.add('mario-scenery',r, 250, 'heaven_cloud')
+    m=wrap.sprite.add('mario-scenery',r, 300, 'heaven_cloud')
     wrap.sprite.set_size_percent(m,50,50)
+    if n==0:
+        n=1
+        mar=wrap.sprite.add('mario-1-small',r+40,250,'jump')
+
 
 s=wrap.sprite.add('mario-items',1000,450,'block_bricks')
 height_sten=wrap.sprite.get_height(s)
@@ -27,11 +32,30 @@ for f in range(100,height_sten*8+100,height_sten):
 
 m=wrap.sprite.add('mario-items',1000,450,'coin')
 width_m=wrap.sprite.get_width(m)
+p=1
+i=1
+
 for y in range(width_m,w,width_m*2):
-    n=width_m*5
+    if p==3:
+        p=4
+        continue
+    if p==0:
+        p=3
+        continue
+    n=width_m*3
     m = wrap.sprite.add('mario-items', y, 200, 'coin')
     g=wrap.sprite.get_x(m)
     if n == g:
-        continue
+        p=0
+
+
+@wrap.on_key_always(wrap.K_RIGHT,wrap.K_LEFT)
+def peredviszenie(keys):
+    if wrap.K_LEFT in keys:
+        wrap.sprite.move(mar,-5,0)
+    if wrap.K_RIGHT in keys:
+        wrap.sprite.move(mar,5,0)
+
+
 
 
---
+++ my_progekt1_mario.py
@@ -0,0 +1,108 @@
+import wrap
+
+
+w=500
+wrap.world.create_world(w,500)
+wrap.world.set_back_color(98,149,248)
+
+e = wrap.sprite.add('mario-enemies',1000 , 450, 'cloud')
+c = wrap.sprite.get_width(e)
+
+for b in range(c//2,w,c):
+    # wrap.sprite.move_to(e,c,0)
+    wrap.sprite.add('mario-enemies',b, 450, 'cloud')
+
+q=wrap.sprite.add('mario-scenery',1000,450,'heaven_cloud')
+i = wrap.sprite.get_width(q)//2
+start=300
+n=0
+# for r in range(start, i * 6 + start, i):
+#     m=wrap.sprite.add('mario-scenery',r, 300, 'heaven_cloud')
+#     wrap.sprite.set_size_percent(m,50,50)
+#     if n==0:
+#         n=1
+
+
+s=wrap.sprite.add('mario-items',1000,450,'block_bricks')
+height_sten=wrap.sprite.get_height(s)
+for f in range(5,height_sten*8+180,height_sten):
+    a=wrap.sprite.add('mario-items',100,f,'block_bricks')
+
+
+
+m=wrap.sprite.add('mario-items',1000,450,'coin')
+width_m=wrap.sprite.get_width(m)
+p=1
+i=1
+
+for y in range(width_m,w,width_m*2):
+    if p==3:
+        p=4
+        continue
+    if p==0:
+        p=3
+        continue
+    n=width_m*3
+    m = wrap.sprite.add('mario-items', y, 200, 'coin')
+    g=wrap.sprite.get_x(m)
+    if n == g:
+        p=0
+
+mar=wrap.sprite.add('mario-1-small',300,275,'jump')
+plot=wrap.sprite.add('mario-scenery',300,300,'cloud1')
+wrap.sprite.set_reverse_y(plot,True)
+@wrap.on_key_always(wrap.K_RIGHT,wrap.K_LEFT)
+def peredviszenie_x(keys):
+    pol_x=wrap.sprite.get_x(plot)
+    pol_y=wrap.sprite.get_y(plot)-25
+    if wrap.sprite.get_left(plot)>100 and wrap.sprite.get_left(plot)<=120:
+        wrap.sprite.move_to(plot,0,300)
+        wrap.sprite.move_to(mar,pol_x,pol_y)
+        return
+
+    if wrap.K_LEFT in keys:
+        wrap.sprite.set_reverse_x(mar,True)
+        wrap.sprite.move(plot,-5,0)
+        wrap.sprite.move_to(mar,pol_x,pol_y)
+    if wrap.K_RIGHT in keys:
+        wrap.sprite.set_reverse_x(mar,False)
+        wrap.sprite.move(plot,5,0)
+        wrap.sprite.move_to(mar,pol_x,pol_y)
+
+
+@wrap.on_key_always(wrap.K_UP,wrap.K_DOWN,wrap.K_SPACE)
+def peredviszenie_y(keys):
+    pol_x=wrap.sprite.get_x(plot)
+    pol_y=wrap.sprite.get_y(plot)-25
+    if wrap.sprite.get_bottom(plot)>=434:
+        wrap.sprite.move(plot,0,-1)
+        return
+    if wrap.sprite.get_y(plot)<=266:
+        wrap.sprite.move(plot,0,1)
+        return
+
+    if wrap.K_UP in keys:
+        wrap.sprite.move(plot,0,-5)
+        wrap.sprite.move_to(mar,pol_x,pol_y)
+
+    if wrap.K_DOWN in keys:
+        wrap.sprite.move(plot,0,5)
+        wrap.sprite.move_to(mar,pol_x,pol_y)
+
+
+@wrap.on_key_down(wrap.K_SPACE)
+def priszok(keys):
+        wrap.actions.move(mar,0,-50,500)
+        wrap.actions.move(mar,0,50,500)
+
+
+
+
+
+
+
+
+
+
+
+
