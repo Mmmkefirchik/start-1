127bad432f7f309f57b6bbdba08abda73e0c56d6
Kirill <mail@mail.com>
2025-09-30 19:34:04 +0300

    fyvfhvfhfhkgukl

--- gribi.py
+++ gribi.py
@@ -2,10 +2,26 @@ import wrap, random
 
 wrap.world.create_world(500, 500)
 
-gribi = [wrap.sprite.add('mario-items', 5, 150, 'star'), wrap.sprite.add('mario-items', 15, 50, 'star'),
-         wrap.sprite.add('mario-items', 10, 100, 'star')]
+gribi = []
+colvo_grib = len(gribi)
 
 
-@wrap.always(5000)
+@wrap.always(1000)
 def poyavlenie():
-    gribi.append(wrap.sprite.add('mario-items', random.randint(0, 500), random.randint(0, 500), 'star'))
+    a={
+        'nomer':wrap.sprite.add('mario-items', random.randint(0, 500), random.randint(0, 500), 'star'),
+        "napravlenie_x":random.randint(-10,10),
+        "napravlenie_y":random.randint(-10,10)
+    }
+    gribi.append(a)
+    print(gribi)
+
+@wrap.always()
+def dvizh ():
+    for dvizh_pravo in gribi:
+
+
+        wrap.sprite.move(dvizh_pravo['nomer'],dvizh_pravo['napravlenie_x'],dvizh_pravo['napravlenie_y'])
+
+
+
