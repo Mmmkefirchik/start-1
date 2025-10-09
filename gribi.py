1164359fc5c73ba7dba010e5a137416fe1b66964
Kirill <mail@mail.com>
2025-10-09 19:43:32 +0300

    fyvfhvfhfhkgukl

--- gribi.py
+++ gribi.py
@@ -1,8 +1,9 @@
 import wrap, random
-
+wrap.add_sprite_dir('sklad')
 wrap.world.create_world(500, 500)
 
 gribi = []
+
 colvo_grib = len(gribi)
 
 
@@ -15,7 +16,7 @@ def poyavlenie():
         "speed_y":random.choice([random.randint(-10,-1),random.randint(1,10)])
     }
     gribi.append(a)
-    print(gribi)
+    # print(gribi)
 
 @wrap.always()
 def dvizh ():
@@ -36,12 +37,37 @@ def dvizh ():
         if wrap.sprite.get_bottom(zvezda['nomer'])>=500:
             zvezda['speed_y']=-abs(zvezda['speed_y'])
 
+
+kolvo_monet=0
+kapital=wrap.sprite.add_text(str(0),50,50,text_color=(255,255,0),font_size=40)
+moneta_na_krane=wrap.sprite.add('proekt_gribi',85,50,'moneta')
+wrap.sprite.set_size_percent(moneta_na_krane,10,10)
 @wrap.on_mouse_down(wrap.BUTTON_LEFT)
 def zbir_zvezd (pos_x,pos_y):
+    global kolvo_monet,pushka
     for nomer_zvezd in gribi:
-        if wrap.sprite.is_collide_point(nomer_zvezd['nomer'],pos_x,pos_y)==True:
+        if wrap.sprite.is_collide_point(nomer_zvezd['nomer'],pos_x,pos_y):
             wrap.sprite.remove(nomer_zvezd['nomer'])
             gribi.remove(nomer_zvezd)
+
+            kolvo_monet=kolvo_monet+1
+            wrap.sprite_text.set_text(kapital,str(kolvo_monet))
+            print(kolvo_monet)
+
+            if kolvo_monet >= 1:
+                pushka=wrap.sprite.add('proekt_gribi',450,50,'pushka')
+
+
+            if wrap.sprite.is_collide_point(pushka, pos_x, pos_y):
+                wrap.sprite.remove(pushka)
+
+
+
+
+
+
+
+
 
 
 
