86e4b5acdfd36087b59aa8f8e60489cc121c5cdd
Kirill <mail@mail.com>
2025-10-28 19:32:02 +0300

    fyvfhvfhfhkgukl

--- ds3.py
+++ ds3.py
@@ -1,8 +1,31 @@
 import wrap
 
-shkolniki=int(input('школьники'))
-yabloki=int(input('яблоки'))
-yabloki_dkya_shkolnika=shkolniki//yabloki*yabloki
+# shkolniki=int(input('школьники'))
+# yabloki=int(input('яблоки'))
+# yabloki_dkya_shkolnika=shkolniki//yabloki*yabloki
+#
+# print(yabloki_dkya_shkolnika)
+# print(shkolniki%yabloki)
 
-print(yabloki_dkya_shkolnika)
-print(shkolniki%yabloki)
+# parti
+# klass_a=int(input('учеников?'))
+# klass_b=int(input('учеников?'))
+# klass_c=int(input('учеников?'))
+#
+# parti_klassa_a=(klass_a%2)+klass_a//2
+# parti_klassa_b=(klass_b%2)+klass_b//2
+# parti_klassa_c=(klass_c%2)+klass_c//2
+#
+# vsego_part=parti_klassa_a+parti_klassa_b+parti_klassa_c
+#
+# print(vsego_part)
+#
+#
+#
+# stranici
+#
+k=int(input('строки?'))#5
+n=int(input('номер строки?'))#15
+
+
+
--- gribi.py
+++ gribi.py
@@ -1,44 +1,67 @@
 import wrap, random
-from wrap_mENdRU.sprite import remove
 
-pushka_ur1=None
+pushka_ur1 = None
 wrap.add_sprite_dir('sklad')
 wrap.world.create_world(500, 500)
 
 gribi = []
 puli = []
+kapital = wrap.sprite.add_text(str(0), 50, 50, text_color=(255, 255, 0), font_size=40)
 
 colvo_grib = len(gribi)
+kolvo_monet = 0
+moneta_na_krane = wrap.sprite.add('proekt_gribi', 85, 50, 'moneta')
+wrap.sprite.set_size_percent(moneta_na_krane, 10, 10)
+
+PUSHKA_NE_PRODAYOTSA = 0
+PUSHKA_V_PRODAZHE = 1
+PUSHKA_KUPLENA = 2
+pushka_zapusk = PUSHKA_NE_PRODAYOTSA
+ugol = 5
+
+check_na_zaskok = False
+x = 0
+y = 0
+distanciya = 5
+def delete_zvezd(x, y):
+    a = poisk_zvezd_po_koordinatam(x, y)
+
+    if a == None:
+        return
+
+    wrap.sprite.remove(a['nomer'])
+
+    gribi.remove(a)
+    return True
+
+def plus_money(dohod=1):
+    global kolvo_monet
+    kolvo_monet = kolvo_monet + dohod
+    wrap.sprite_text.set_text(kapital, str(kolvo_monet))
+
+
 
 def poisk_zvezd_po_koordinatam(x, y):
     for zvezda in gribi:
         if wrap.sprite.is_collide_point(zvezda['nomer'], x, y):
             return zvezda
-def zbivanie_zvezd ():
 
+
+def zbivanie_zvezd():
     if pushka_zapusk != PUSHKA_KUPLENA:
         return
 
     for pula in puli.copy():
-        x_pola=wrap.sprite.get_x(pula['nomer'])
-        y_pola=wrap.sprite.get_y(pula['nomer'])
+        x_pola = wrap.sprite.get_x(pula['nomer'])
+        y_pola = wrap.sprite.get_y(pula['nomer'])
 
-        zvezda_mertva=poisk_zvezd_po_koordinatam(x_pola,y_pola)
-        if zvezda_mertva == None:
+        if delete_zvezd(x_pola, y_pola) == None:
             continue
-
-        wrap.sprite.remove(zvezda_mertva['nomer'])
-        gribi.remove(zvezda_mertva)
 
         wrap.sprite.remove(pula['nomer'])
         puli.remove(pula)
-# for zvezdi in gribi.copy():
-        #     if wrap.sprite.is_collide_point(zvezdi['nomer'],x_pola,y_pola):
-        #         wrap.sprite.remove(zvezdi['nomer'])
-        #         gribi.remove(zvezdi)
-        #         wrap.sprite.remove(pula['nomer'])
-        #         puli.remove(pula)
-        #         break
+        plus_money()
+
 
 @wrap.always(1000)
 def poyavlenie():
@@ -71,117 +94,101 @@ def dvizh():
     zbivanie_zvezd()
 
 
-kolvo_monet = 0
-kapital = wrap.sprite.add_text(str(0), 50, 50, text_color=(255, 255, 0), font_size=40)
-moneta_na_krane = wrap.sprite.add('proekt_gribi', 85, 50, 'moneta')
-wrap.sprite.set_size_percent(moneta_na_krane, 10, 10)
 
-PUSHKA_NE_PRODAYOTSA=0
-PUSHKA_V_PRODAZHE=1
-PUSHKA_KUPLENA=2
-pushka_zapusk = PUSHKA_NE_PRODAYOTSA
 
 
 @wrap.on_mouse_down(wrap.BUTTON_LEFT)
 def zbor_zvezd(pos_x, pos_y):
     global kolvo_monet, pushka_knopka, pushka_zapusk
 
-    zvezda = poisk_zvezd_po_koordinatam(pos_x, pos_y)
-    if zvezda == None:
+    if delete_zvezd(pos_x, pos_y) == None:
         return
 
-    wrap.sprite.remove(zvezda['nomer'])
-    gribi.remove(zvezda)
-    kolvo_monet = kolvo_monet + 1
-    wrap.sprite_text.set_text(kapital, str(kolvo_monet))
-
-    if kolvo_monet >= 1 and pushka_zapusk == PUSHKA_NE_PRODAYOTSA:
+    plus_money()
+    if kolvo_monet >= 10 and pushka_zapusk == PUSHKA_NE_PRODAYOTSA:
         pushka_knopka = wrap.sprite.add('proekt_gribi', 450, 50, 'pushka')
 
         pushka_zapusk = PUSHKA_V_PRODAZHE
 
 
-
-
 @wrap.on_mouse_down(wrap.BUTTON_LEFT)
 def prodasza_pushki(pos_x, pos_y):
-    global pushka_zapusk, pushka_ur1
+    global pushka_zapusk, pushka_ur1,kolvo_monet
 
-    if pushka_zapusk == PUSHKA_KUPLENA or pushka_zapusk==PUSHKA_NE_PRODAYOTSA:
+    if pushka_zapusk == PUSHKA_KUPLENA or pushka_zapusk == PUSHKA_NE_PRODAYOTSA:
         return
-
     if wrap.sprite.is_collide_point(pushka_knopka, pos_x, pos_y):
         wrap.sprite.remove(pushka_knopka)
 
-        pushka_ur1=wrap.sprite.add('proekt_gribi',250,500,'bashna1')
-        wrap.sprite.set_size_percent(pushka_ur1,45,45)
-        wrap.sprite.set_angle(pushka_ur1,0)
+        pushka_ur1 = wrap.sprite.add('proekt_gribi', 250, 500, 'bashna1')
+        wrap.sprite.set_size_percent(pushka_ur1, 45, 45)
+        wrap.sprite.set_angle(pushka_ur1, 0)
         pushka_zapusk = PUSHKA_KUPLENA
 
-ugol=5
+        plus_money(-10)
 
-check_na_zaskok = False
+
+
 
 
 @wrap.always()
-def strelba_is_pushki (pos_x,pos_y):
-    global ugol,check_na_zaskok
-    if pushka_zapusk!=PUSHKA_KUPLENA:
+def strelba_is_pushki(pos_x, pos_y):
+    global ugol, check_na_zaskok
+    if pushka_zapusk != PUSHKA_KUPLENA:
         return
 
-    ugol_pushki=wrap.sprite.get_angle(pushka_ur1)
-    if ugol_pushki>=90:
-        check_na_zaskok=True
-    if ugol_pushki<-90:
-        check_na_zaskok=False
+    ugol_pushki = wrap.sprite.get_angle(pushka_ur1)
+    if ugol_pushki >= 90:
+        check_na_zaskok = True
+    if ugol_pushki < -90:
+        check_na_zaskok = False
 
-    if check_na_zaskok==False:
+    if check_na_zaskok == False:
         ugol = ugol + 5
 
-    if check_na_zaskok==True:
+    if check_na_zaskok == True:
         ugol = ugol - 5
 
     wrap.sprite.set_angle(pushka_ur1, ugol)
 
-x=0
-y=0
+
+
+
+
 @wrap.always(1000)
-def strelba ():
-    if pushka_zapusk!=PUSHKA_KUPLENA:
+def strelba():
+    if pushka_zapusk != PUSHKA_KUPLENA:
         return
 
-    ugol_pushki=wrap.sprite.get_angle(pushka_ur1)
-    x_y=wrap.sprite.get_pos(pushka_ur1)
+    ugol_pushki = wrap.sprite.get_angle(pushka_ur1)
+    x_y = wrap.sprite.get_pos(pushka_ur1)
 
-    pula=wrap.sprite.add('pacman',x_y[0],x_y[1],'dot')
-    pula_coordinat= {'nomer':pula,
-           'ugol':ugol_pushki}
+    pula = wrap.sprite.add('pacman', x_y[0], x_y[1], 'dot')
+    pula_coordinat = {'nomer': pula,
+                      'ugol': ugol_pushki}
 
     puli.append(pula_coordinat)
 
+    wrap.sprite.move_at_angle(pula, ugol_pushki, 140)
 
-    wrap.sprite.move_at_angle(pula,ugol_pushki,140)
 
-distanciya=5
+
+
+
 @wrap.always()
-def polet_pul ():
+def polet_pul():
     global distanciya
     for pula in puli:
-        xy=wrap.sprite.get_pos(pula['nomer'])
+        xy = wrap.sprite.get_pos(pula['nomer'])
 
-        if xy[0]>=500 or xy[0]<0 or xy[1]<0:
+        if xy[0] >= 500 or xy[0] < 0 or xy[1] < 0:
             puli.remove(pula)
 
-
-        wrap.sprite.move_at_angle(pula['nomer'],pula['ugol'],distanciya)
+        wrap.sprite.move_at_angle(pula['nomer'], pula['ugol'], distanciya)
         print(len(puli))
     zbivanie_zvezd()
 
 
-
-
-
-
-
 import wrap_py
+
 wrap_py.app.start()
